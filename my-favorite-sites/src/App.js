98045078115ea989d9efe12087328e4c95f23180
artemesan <artesuto@gmail.com>
2020-02-28 10:59:09 +0700

    add full function for create bookmark list

--- my-favorite-sites/src/App.js
+++ my-favorite-sites/src/App.js
@@ -3,35 +3,66 @@ import './App.css';
 import BookmarkComponent, { getBookmarkNumber} from './BookmarkComponents';
 
 class App extends Component{
+  currentBookmark = [];
   constructor(props){
     super(props);
-    this.state = {number: getBookmarkNumber(), bookmarkList: []};
-    this.saveCurrent = this.saveCurrent.bind(this);
+    this.state = {number: getBookmarkNumber(), bookmarkList: this.getCurrent()};
+    this.setCurrent = this.setCurrent.bind(this);
+    this.changeCurrent = this.changeCurrent.bind(this);
+    this.onListChanged = this.onListChanged.bind(this);
+    this.addtoCurrent = this.addtoCurrent.bind(this);
+    this.removefromCurrent = this.removefromCurrent.bind(this);
+    
+    
+    this.currentBookmark = this.getCurrent();
+  }
+
+// On change bookmark list
+  onListChanged(){
+    this.setState({bookmarkList: this.currentBookmark}, this.setCurrent);
   }
 // Load current bookmarks into state
-  loadCurrent(){
+  getCurrent(){
     var curBookmarkList = JSON.parse(localStorage.getItem("BookmarkList"));
     return curBookmarkList;
   }
 // Save state into local storage
-  saveCurrent(){
-    var curBookmarkList = JSON.stringify(this.state.bookmarkList);
+  setCurrent(){
+    var curBookmarkList = JSON.stringify(this.currentBookmark);
     localStorage.setItem("BookmarkList",curBookmarkList);
+  }
+// Handle edit or delete bookmark
+  changeCurrent(id,newlink){
+    this.currentBookmark[id] = newlink;
+    this.onListChanged();
+  }
+  // Remove bookmark
+  removefromCurrent(id){
+    this.currentBookmark.splice(id,1);
+    this.onListChanged();
+  }
+
+// Add new item to current bookmark
+  addtoCurrent(){
+    var newlink = prompt("Enter new link");
+    if ((newlink === null) || (newlink === "")) return newlink;
+    this.currentBookmark[this.currentBookmark.length] = newlink;
+    this.onListChanged();
   }
 
   render(){
     // create bookmark list from state
     let bookmarkList = [];
-    for (let i=1; i<= this.state.number;i++){
-      bookmarkList.push(<BookmarkComponent link="vnexpress.vn"/>);
+    for (let i=0; i<= (this.state.bookmarkList.length-1);i++){
+      bookmarkList.push(<BookmarkComponent id={i} link={this.state.bookmarkList[i]} edit={this.changeCurrent} delete={this.removefromCurrent}/>);
     }
 
     return (
       <div className="App">
         {bookmarkList}
         <span>{this.state.number}</span>
-        <div class = "btn btn-info p-2 m-2">Add</div>
-        <div class = "btn btn-info p-2 m-2" onClick = {this.saveCurrent}>Save</div>
+        <div className = "btn btn-info p-2 m-2" onClick = {this.addtoCurrent}>Add</div>
+        <div className = "btn btn-info p-2 m-2" onClick = {this.setCurrent}>Save</div>
       </div>
     );
   }
--- my-favorite-sites/src/BookmarkComponents.js
+++ my-favorite-sites/src/BookmarkComponents.js
@@ -4,28 +4,35 @@ class BookmarkComponent extends Component{
     constructor(props){
       super(props);
       this.edit = this.edit.bind(this);
-      this.state = {link: props.link};
+      this.delete = this.delete.bind(this);
+      this.state = {id: props.id, link: props.link};
     }
   
     edit(){
       var newlink = prompt("Enter new link", this.props.link);
-      if ((newlink == null) || (newlink == "")) return newlink;
-      this.setState({link: newlink});
+      if ((newlink === null) || (newlink === "")) return newlink;
+      this.setState({link: newlink}, () => {this.props.edit(this.state.id, newlink)});
+    }
+
+    delete(){
+      this.props.delete(this.state.id);
     }
   
     render(){
       return (
         <React.Fragment>
-          <a class="btn btn-dark p2 m2" href={"//" + this.state.link}>{this.state.link}</a>
-          <div class="btn btn-info" onClick={this.edit}> Edit</div>
+          <span>{this.state.id}</span>
+          <a className="btn btn-dark p-2 m-2" href={"//" + this.state.link}>{this.state.link}</a>
+          <span className="btn btn-info m-2 p-2" onClick={this.edit}> Edit</span>
+          <span className="btn btn-info m-2 p-2" onClick={this.delete}> Delete</span>
           <br></br>
         </React.Fragment>
       )
     }
   }
   
-  export function getBookmarkNumber(){
+export function getBookmarkNumber(){
     return localStorage.length;
-  }
+}
 
-  export default BookmarkComponent;
+export default BookmarkComponent;
